4777bbb3af1ef29a21c2953ba17781bc764b9f98
Minh Nguyen <minhnpa1907@gmail.com>
2016-10-21 16:51:22 +0700

    done with article model

---
+++ app/src/main/java/com/minhnpa/coderschool/newyorktimesarticlesearch/model/Article.java
@@ -0,0 +1,49 @@
+package com.minhnpa.coderschool.newyorktimesarticlesearch.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class Article implements Parcelable{
+    @SerializedName("web_url")
+    private String webUrl;
+
+    @SerializedName("snippet")
+    private String snippet;
+
+    @SerializedName("multimedia")
+    private List<Media> multimedia;
+
+    protected Article(Parcel in) {
+        webUrl = in.readString();
+        snippet = in.readString();
+        multimedia = in.createTypedArrayList(Media.CREATOR);
+    }
+
+    public static final Creator<Article> CREATOR = new Creator<Article>() {
+        @Override
+        public Article createFromParcel(Parcel in) {
+            return new Article(in);
+        }
+
+        @Override
+        public Article[] newArray(int size) {
+            return new Article[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(webUrl);
+        dest.writeString(snippet);
+        dest.writeTypedList(multimedia);
+    }
+}
---
+++ app/src/main/java/com/minhnpa/coderschool/newyorktimesarticlesearch/utils/Constant.java
@@ -0,0 +1,9 @@
+package com.minhnpa.coderschool.newyorktimesarticlesearch.utils;
+
+/**
+ * Created by MINH NPA on 21 Oct 2016.
+ */
+
+public class Constant {
+    public static final String BASE_URL = "http://www.nytimes.com/";
+}
